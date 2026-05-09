e2f7927e482a2468acf471b76be2d11a89f1232e
HelenDiMo <elena.diazmoreno@gmail.com>
2026-05-09 18:11:43 +0200

    feat(etl-extract): refactor extract to load registros_climaticos_normalizados.json with Spanish logs

--- etl/extract.py
+++ etl/extract.py
@@ -2,25 +2,26 @@
 import json
 import os # Esto sirve para que el código buscar localizar el archivo .py donde sea que esté
 
-def extraer_datos(ruta_archivo):
-    directorio_actual = os.path.dirname(__file__) # Esto busca la carpeta donde está este script (extract.py)
-    ruta_completa = os.path.join(directorio_actual, ruta_archivo) # ruta absoluta al archivo JSON
+def extract_data(relative_path):
+    current_dir = os.path.dirname(__file__) # Esto busca la carpeta donde está este script (extract.py)
+    full_path = os.path.join(current_dir, relative_path) # ruta absoluta al archivo JSON
 
     try:
-        with open(ruta_completa, 'r') as file: # lee el archivo JSON
-            datos = json.load(file) # convierte el JSON en lenguaje Python (lista de diccionarios)
+        with open(full_path, 'r', encoding="utf-8") as file: # lee el archivo JSON
+            data = json.load(file) # convierte el JSON en lenguaje Python (lista de diccionarios)
             print("✅ Extracción exitosa") 
-            return datos # devuelve los datos para que puedan ser usados por otros scripts
+            return data # devuelve los datos para que puedan ser usados por otros scripts
     except Exception as e: # si ocurre un error, lo captura y muestra un mensaje (e)
         print(f"❌ Error al leer el archivo: {e}")
-
-# --- ESTO ES PARA PROBAR QUE FUNCIONA ---
-if __name__ == "__main__":
-    ruta = "../data/registros_climaticos.json" # El '..' significa 'sal de la carpeta etl y busca fuera'
-    mis_datos = extraer_datos(ruta)
+        return None
     
-    if mis_datos:
-        print(f"He encontrado {len(mis_datos)} registros.")
-        print(f"El primer registro es de: {mis_datos[0]['estacion_id']}") # la variable "estacion_id" queda pendiente de denominar
+# --- ESTO ES PARA PROBAR MANUALMENTE QUE FUNCIONA ---
+if __name__ == "__main__":
+    path = "../data/registros_climaticos_normalizados.json" # El '..' significa 'sal de la carpeta etl y busca fuera'
+    data = extract_data(path)
+    
+    if data:
+        print(f"He encontrado {len(data)} registros.")
+        print(f"El primer registro es de: {data[0]['estacion_id']}") # la variable "estacion_id" queda pendiente de denominar
 
         
